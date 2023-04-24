d63a86830833b7b5495c2bf58df1215d7da78dfb
Stephenchuks <stephenchuks@my.uopeople.edu>
2023-04-24 08:30:37 -0700

    hot fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
         user_id = user['id']
         url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
         tasks = requests.get(url).json()
-        all_tasks[user_id] = [{"username": user['username'], "task": task['title'], "completed": task['completed']} for task in tasks]
+        all_tasks[user_id] = [{"username": user['username'],
+            "task": task['title'], 
+            "completed": task['completed']} for task in tasks]
 
     with open('todo_all_employees.json', 'w') as f:
         json.dump(all_tasks, f)
